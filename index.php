35defcf60de08930c3fc4d29b14db6c7b77a881b
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-09 13:18:12 -0300

    Add index, navbar and styles

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles/index-style.min.css">
+    <title>La Nación</title>
+
+  </head>
+  <body>
+    <?php include 'navbar.php'; ?>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,28 @@
+<header>
+  <div class="nav-top">
+    <div class="titulo">
+      <h1>LA NACION</h1>
+      <p class="fecha"></p>
+      <div class="clima">
+        <img class="icono-clima" src="" alt="">
+        <p class="temperatura"></p>
+      </div>
+    </div>
+    <div class="btn-ingresar-subscribete">
+      <a class="ingresar" href="#">Ingresar</a>
+      <a class="subscribete" href="#">Subscribete</a>
+      <a class="lupa" href="#"><img src="" alt=""></a>
+    </div>
+  </div>
+  <div class="nav-bottom">
+    <a class="hoy" href="#">Hoy</a>
+    <a class="actualidad" href="#">Actualidad</a>
+    <a class="negocios" href="#">Negocios</a>
+    <a class="deportes" href="#">Deportes</a>
+    <a class="vida-ocio" href="#">Vida&ocio</a>
+    <a class="ideas" href="#">Ideas</a>
+    <a class="espectaculos" href="#">Espectaculos</a>
+    <a class="dolar-hoy" href="#">Dolar Hoy</a>
+    <a class="servicios" href="#">Servicios</a>
+  </div>
+</header>
