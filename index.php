aa6eb1a46c461e3dca2ce8c78b5c2f1785d3d5a7
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-11 18:51:11 -0300

    Add import oftext style, Add section two

--- index.php
+++ index.php
@@ -2,18 +2,56 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <!-- link navbar style -->
     <link rel="stylesheet" href="styles/navbar-style.min.css">
+    <!-- link main news style (News with big image at the top of the landing page) -->
     <link rel="stylesheet" href="styles/main-news-style.min.css">
+    <!-- Link three main news style (Three news on the second section) -->
+    <link rel="stylesheet" href="styles/three-main-news-style.min.css">
+    <!-- Linking all google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet">
+    <!-- Meta tag for responsive website -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Adding LN logo to title -->
     <link rel="shortcut icon" href="images/logo.png"/>
     <title>LA NACION</title>
-
   </head>
   <body>
+    <!-- Include navbar to landing -->
     <?php include 'navbar.php'; ?>
+    <!-- Include first section of the page -->
     <?php include 'main-news.php'; ?>
+    <!-- Include second section of the page -->
+    <?php include 'three-main-news.php' ?>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </body>
 </html>
---
+++ three-main-news.php
@@ -0,0 +1,35 @@
+<div class="section-two">
+  <div class="container">
+    <!-- First News Block -->
+    <div class="news-container">
+      <img src="images/news-picture.png" alt="picture">
+      <div class="text-box">
+        <a class="fecha-noticia" href="#">DOMINGO  04  DE  JUNIO  DE  2017</a>
+        <a class="titulo" href="#">CRISTINA, EL PLAN SECRETO DE MACRI</a>
+        <p class="parrafo">La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ...La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ... </p>
+        <a class="autor" href="#">Por Joaquín Morales Solá<a href="#">
+      </div>
+    </div>
+    <!-- Second News Block -->
+    <div class="news-container">
+      <img src="images/news-picture.png" alt="picture">
+      <div class="text-box">
+        <a class="fecha-noticia" href="#">DOMINGO  04  DE  JUNIO  DE  2017</a>
+        <a class="titulo" href="#">CRISTINA, EL PLAN SECRETO DE MACRI</a>
+        <p class="parrafo">La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ... La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ...La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ...</p>
+        <a class="autor" href="#">Por Joaquín Morales Solá<a href="#">
+      </div>
+    </div>
+    <!-- Third News Block -->
+    <div class="news-container">
+      <img src="images/news-picture.png" alt="picture">
+      <div class="text-box">
+        <a class="fecha-noticia" href="#">DOMINGO  04  DE  JUNIO  DE  2017</a>
+        <a class="titulo" href="#">CRISTINA, EL PLAN SECRETO DE MACRI</a>
+        <p class="parrafo">La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ... </p>
+        <a class="autor" href="#">Por Joaquín Morales Solá<a href="#">
+      </div>
+    </div>
+  </div>
+
+</div>
