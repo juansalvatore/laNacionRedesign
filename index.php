dacc5822236a658ff5102cca5930008e306e77c3
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-11 16:37:54 -0300

    Add main news, styles, correct fixed navbar

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="styles/index-style.min.css">
+    <link rel="stylesheet" href="styles/navbar-style.min.css">
+    <link rel="stylesheet" href="styles/main-news-style.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="images/logo.png"/>
     <title>LA NACION</title>
@@ -10,5 +14,35 @@
   </head>
   <body>
     <?php include 'navbar.php'; ?>
+    <?php include 'main-news.php'; ?>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </body>
 </html>
---
+++ main-news.php
@@ -0,0 +1,15 @@
+<div class="section">
+  <div class="container">
+    <div class="image-container">
+      <a href="#"><img src="images/img-main-news.png" alt="Macri"></a>
+    </div>
+    <div class="text-container">
+      <div class="text-box">
+        <a class="fecha-noticia" href="#">DOMINGO  04  DE  JUNIO  DE  2017</a>
+        <a class="titulo" href="#">CRISTINA, EL PLAN SECRETO DE MACRI</a>
+        <p class="parrafo">La prensa británica asegura que hay un plan secreto en Argentina para las Malvinas ... </p>
+        <a class="autor" href="#">Por Joaquín Morales Solá<a href="#">
+      </div>
+    </div>
+  </div>
+</div>
